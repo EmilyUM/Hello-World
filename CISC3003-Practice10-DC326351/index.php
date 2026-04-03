7c27d0dfb85336553a5e1f9fcfc1eab0a8114287
EmilyUM <dc32635@um.edu.mo>
2026-04-03 23:00:51 +0800

    Add files via upload
    UPLOAD PRACTICE10

---
+++ CISC3003-Practice10-DC326351/index.php
@@ -0,0 +1,36 @@
+
+<html>
+<head>
+    <title>DC326351 FAN ZOU CHEN PHP</title>
+</head>
+<body>
+    <h1>FAN ZOU CHEN PHP</h1>
+    <?php
+    $name = "FAN ZOU CHEN"; 
+    $hash = hash('sha256', $name);
+    echo "<p>SHA256 of '$name' is: $hash</p>";
+    ?>
+
+    <pre>
+        ************
+        **        **
+        **
+        **
+        **
+        **        **
+        ************
+    </pre>
+
+    
+</body>
+</html>
+
+
+    
+    <p>Check the links below:</p>
+    <ul>
+        <li><a href="fail.php">Click here to check error setting</a></li>
+        <li><a href="check.php"> Click here to cause a trace back</a></li>
+    </ul>
+</body>
+</html>
